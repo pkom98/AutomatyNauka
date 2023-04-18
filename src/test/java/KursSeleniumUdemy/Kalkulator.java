ac2d66fd5411b3202a688fb93517be1443dbd2c2
Pawel <pkom98@wp.pl>
2023-04-18 22:24:03 +0200

    Add Kalkulator to repo

---
+++ src/test/java/KursSeleniumUdemy/Kalkulator.java
@@ -0,0 +1,22 @@
+package KursSeleniumUdemy;
+
+import java.util.Scanner;
+
+public class Kalkulator {
+    public static void main(String[] args) {
+
+       Scanner liczba = new Scanner(System.in);
+
+        System.out.println("Podaj pierwszą liczbę: ");
+        int firstNumber = liczba.nextInt();
+        System.out.println("Podaj drugą liczbę: ");
+        int secondNumber = liczba.nextInt();
+        int resultDodawania = firstNumber+secondNumber;
+        int resultMnozenia = firstNumber*secondNumber;
+        float resultDzielenia = (float) firstNumber /secondNumber;
+        System.out.println("Dodawanie: " + resultDodawania);
+        System.out.println("Mnozenie: " + resultMnozenia);
+        System.out.println("Dzielenie: " + resultDzielenia);
+
+    }
+}
