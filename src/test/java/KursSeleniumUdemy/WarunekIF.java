b7502294b85d0731e21482eb3f5aa75c3a86a5c1
Pawel <pkom98@wp.pl>
2023-04-19 14:43:00 +0200

    If statement ref

---
+++ src/test/java/KursSeleniumUdemy/WarunekIF.java
@@ -0,0 +1,15 @@
+package KursSeleniumUdemy;
+
+public class WarunekIF {
+    public static void main(String[] args) {
+
+        float a = 10;
+        float b = 3;
+
+        if (b!=0){
+            System.out.println("Wynik dzielenia to: " + a/b);
+        } else {
+            System.out.println("Nie można dzielic przez 0!!!");
+        }
+    }
+}
