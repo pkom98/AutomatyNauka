b778462262bdd255dcc2364ce5acf3ac61c5bf8d
Pawel <pkom98@wp.pl>
2023-05-02 22:02:28 +0200

    Do While loop

---
+++ src/test/java/KursSeleniumUdemy/DoWhile.java
@@ -0,0 +1,14 @@
+package KursSeleniumUdemy;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int number = 1;
+
+        do {
+            System.out.println("Numer: " + number);
+            number++;
+        }
+        while (number<20);
+    }
+}
