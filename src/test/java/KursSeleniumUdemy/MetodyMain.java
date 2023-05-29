e8bae14a6ad9794af80810efb5603e13cc5b47a9
Pawel <pkom98@wp.pl>
2023-05-29 19:13:22 +0200

    metody main update

---
+++ src/test/java/KursSeleniumUdemy/MetodyMain.java
@@ -0,0 +1,24 @@
+package KursSeleniumUdemy;
+
+import java.util.Scanner;
+
+public class MetodyMain {
+    public static void main(String[] args) throws InterruptedException {
+
+        Metody metody = new Metody();
+
+
+//        metody.komunikat();
+//        metody.scannerAndWynik();
+//        metody.negOrPos();
+//        metody.returnWynik();
+
+//        metody.policzWynikParam(5);
+//        metody.policzWynikParamScanner();
+          metody.showNumber(5);
+          metody.add(5,10);
+
+
+
+    }
+        }
