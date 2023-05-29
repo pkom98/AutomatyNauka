4851b5a3117a1d6cdeb58ec90c47030f0419b9df
Pawel <pkom98@wp.pl>
2023-05-29 19:12:41 +0200

    calculator update

---
+++ src/test/java/KursSeleniumUdemy/KalkulatorMain.java
@@ -0,0 +1,20 @@
+package KursSeleniumUdemy;
+
+import java.util.Scanner;
+
+public class KalkulatorMain {
+    public static void main(String[] args) throws InterruptedException {
+
+        Metody metody = new Metody();
+
+        metody.welcomeToCalculator();
+        metody.giveNumbers();
+        metody.yourNumbers();
+        metody.options();
+
+
+
+
+
+    }
+}
